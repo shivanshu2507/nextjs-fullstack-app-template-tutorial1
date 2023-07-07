0d02c12bf53c2abc785b7e807404d00d5de015bb
shivanshu2507 <shivanshuverma7697@gmail.com>
2023-07-08 02:47:46 +0530

    feat: add BaseTemplate component

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  /** Expose public folder to storybook as static */
+  staticDirs: ['../public'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
