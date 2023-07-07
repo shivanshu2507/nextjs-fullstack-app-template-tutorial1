b91c934faed106c40bad25bad82818ffa504f6b4
shivanshu2507 <shivanshuverma7697@gmail.com>
2023-07-07 18:59:26 +0530

    build: add debugging configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import './globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
+export default App;
